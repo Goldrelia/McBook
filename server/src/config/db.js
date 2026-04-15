2111e8eff90e6b261a660979f37152fbb30b457b
Derek Long <dikarilong@gmail.com>
2026-04-15 14:37:04 -0400

    Connected to MySQL locally using port 3306(successful), can finally login!

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,8 +2,9 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host:     process.env.DB_HOST || 'localhost',
-  user:     process.env.DB_USER || 'cs307-user',
+  host: process.env.DB_HOST || '127.0.0.1',
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER || 'cs307-user',
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME || 'comp307_booking',
   waitForConnections: true,
